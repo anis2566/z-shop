691e0931548fe8fc83f51e4f2a37dd1e1aecbe24
anis2566 <anichuranis1000@gmail.com>
2024-05-23 19:43:33 +0600

    www

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -74,7 +74,7 @@ export async function POST(req: Request) {
         },
       });
 
-      knock.users.identify(user.id, {
+      await knock.users.identify(user.id, {
         name: user.name,
         avatar: user.imageUrl,
       })
